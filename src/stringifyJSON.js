03a4ffc4dd0ad9a80bf3ee63ca166926f80710ec
Hack Reactor Student <student@hackreactor.com>
2018-06-11 12:38:57 -0700

    First draft before lunch

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,66 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  //check if obj is boolean, number, string, array, obj
+  var str = '';
+  if (typeof obj === "function" || obj === undefined) {
+    return '{}';
+  } else if (typeof obj === 'string') {
+    str += '\"' + obj.toString() + '\"';
+    return str;
+  } else if (typeof obj === 'boolean' || typeof obj === 'number') {
+    str = obj.toString();
+    return str;
+  } else if (obj === null || obj === Infinity || obj === NaN) {
+    return 'null';
+  } else if (Array.isArray(obj)) {
+    //create new array 
+    //iterate over each item in the array calling stringifyJSON on each item
+    //add to str 
+    // make sure to add brackets to str first
+    str += '[';
+    obj.forEach(function(value) {
+      var addToStr = stringifyJSON(value);
+      str += addToStr + ',';
+    });
+    if (str.length > 1) {
+      var slicedStr = str.slice(0, (str.length - 1));
+      slicedStr += ']';
+      return slicedStr;
+    } else {
+      str += ']';
+      return str;
+    }
+    
+
+    // var slicedStr = str.slice(0, (str.length - 1));
+    // slicedStr += ']';
+    // return slicedStr;
+
+  } else if (typeof obj === 'object') {
+    str += '{';
+    // console.log(str, 'str1');
+    for (var key in obj) {
+      var keyStr = stringifyJSON(key);
+      var valueStr = stringifyJSON(obj[key]);
+      str += keyStr + ':' + valueStr + ',';
+    // console.log(str, 'str2');
+    }
+    if (str.length > 1) {
+      var slicedStr = str.slice(0, (str.length - 1));
+      slicedStr += '}';
+      return slicedStr;
+    } else {
+      str += '}';
+      return str;
+    } 
+
+    // else if {
+    // }
+    //     str = str.slice(0, (str.length - 1));
+    //   }
+    // console.log(str, 'str3');
+    
+  }
+  return str;
 };
